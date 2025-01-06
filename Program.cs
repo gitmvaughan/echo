8f24c84aff33c0bcade60ff4a19841f187bbf3cb
gitmvaughan <mvaughan@getfasolutions.com>
2025-01-06 10:25:00 -0600

    Initial version

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+string versionText = "Echo Version: 1.0.1 January 6, 2025";
+Console.WriteLine(versionText);
+Console.WriteLine(args[0]);
